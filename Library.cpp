b41e87d3570a6e34ed10cfa40c682d15bb1098f7
Amina khamis <aminakhamissaid19@gmail.Com>
2026-02-02 12:32:04 +0300

    Update Library.cpp

--- Library.cpp
+++ Library.cpp
@@ -1,4 +1,6 @@
-
+outFile << book.bookID << "|" << book.title << "|" << book.author << "|" << book.issued << "\n";//Writing to the file
+    outFile.close(); //Closing the file after writing on it
+              //comments
         inFile.ignore();
         getline(inFile, book.title, '|');
         getline(inFile, book.author, '|');
